0100673e2cd7c9a7972e04b8404ac8e156068e7a
risingstar8030 <surajvyavhare1198@gmail.com>
2019-08-06 19:03:51 +0530

    commit

---
+++ DiffrenceArray.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+bool map_compare (map< ll,ll > const &lhs, map< ll,ll > const &rhs);
+int main()
+{
+    ll t;
+    cin>>t;
+    while (t--)
+    {
+       ll n,ip;
+       cin>>n;
+       vector<ll> C(n),H(n),level(n,0);
+       map< ll,ll > mp,mp1;
+       map< ll,ll > :: iterator itr1,itr2;
+    
+       for(ll i=0;i<n;i++)
+       {
+           cin>>ip;
+           C[i] = ip;
+       }
+       for(ll i=0;i<n;i++)
+       {
+           cin>>ip;
+           H[i] = ip;
+           mp[ip]++;
+       }
+        for(ll i=0;i<n;i++)
+       {
+            ll d1 = (i+1) - C[i];
+            if(d1 <= 0)
+             {
+                d1 = 0;
+                level[d1]++;
+             }
+             else
+             {
+                d1 = d1-1;
+                level[d1]++;
+             }
+             
+            ll d2 = (i+1) + C[i];
+            if(d2 < n)
+            {
+                level[d2]--;
+            }       
+       }
+      
+        for(ll i=0;i<n;i++)
+        {
+            if(i == 0)
+                level[i] = level[0];
+            else
+            {
+                level[i] = level[i] + level[i-1];
+            }
+            mp1[level[i]]++;
+        }  
+      
+        
+        bool flag = map_compare(mp1,mp);
+        if(!flag)
+            cout<<"NO\n";
+        else
+            cout<<"YES\n";
+
+    }
+    
+}
+
+bool map_compare (map< ll,ll > const &lhs, map< ll,ll > const &rhs) {
+    return lhs.size() == rhs.size()
+        && equal(lhs.begin(), lhs.end(),
+                      rhs.begin());
+}
